c2f891625b82ae7c9b190981ffd9c202bb09e6fa
Sachin Parmar <sachinparmar4160@gmail.com>
2024-10-01 12:10:07 +0530

    added new functionalities like reading a file using cat

--- app/main.js
+++ app/main.js
@@ -1,16 +1,21 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require('zlib')
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const command = process.argv[2];
+const hash = process.argv[4];
 
 switch (command) {
   case "init":
     createGitDirectory();
     break;
+    case "cat-file":
+        catFile(hash);
+        break;
   default:
     throw new Error(`Unknown command ${command}`);
 }
@@ -22,4 +27,11 @@ function createGitDirectory() {
 
   fs.writeFileSync(path.join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
   console.log("Initialized git directory");
+}
+
+function catFile(hash) {
+    const content = fs.readFileSync(path.join(process.cwd(), ".git", "objects", hash.slice(0,2), hash.slice(2)));
+    const dataUnzipped = zlib.inflateSync(content);
+    const result = dataUnzipped.toString().split('\0')[1];
+    process.stdout.write(result);
 }
